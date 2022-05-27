014642434193ab6b2e31cd792bbf849ffc68c357
Hiral Choksi <106080831+hiral17@users.noreply.github.com>
2022-05-28 00:38:16 +0530

    Add files via upload

---
+++ PYTHON PROJECTS/first-project.py
@@ -0,0 +1,26 @@
+first= input("enter first no: ")
+operator =input("enter operator (+,-,/,*,%) :")
+second = input('enter second no: ')
+
+first = int(first)
+second = int(second)
+
+if operator == "+" :
+    print(first + second)
+
+elif operator == "-" :
+    print(first + second)
+
+elif operator == "*" :
+    print(first + second)
+
+elif operator == "/" :
+    print(first + second)
+
+elif operator == "%" :
+    print(first + second)
+
+else:
+    print("invalid operation")
+
+
